4b5191d1f3e515db38485e0b5e5e9579f45b54fc
Hiram Montesillo <montesillo25@gmail.com>
2020-05-18 23:25:08 -0500

    Comparaciones

---
+++ clase8.js
@@ -0,0 +1,36 @@
+var hiram = {
+    nombre: 'Hiram',
+    apellido: 'Montesillo',
+    edad: 28
+};
+
+var dario = {
+    nombre: 'Darío',
+    apellido: 'Susnisky',
+    edad: 27
+};
+
+function imprimirNombreEnMayusculas(persona){
+    var { nombre } = persona;
+    console.log(nombre.toUpperCase());
+}
+
+imprimirNombreEnMayusculas(hiram);
+imprimirNombreEnMayusculas(dario);
+imprimirNombreEnMayusculas({nombre: 'Pepito'});
+//imprimirNombreEnMayusculas();
+
+function imprimirNombreYEdad(persona){
+    var {nombre} = persona;
+    var {edad} = persona;
+    console.log(`Hola me llamo ${nombre} y tengo ${edad} años`);
+}
+
+imprimirNombreYEdad(hiram);
+
+function cumpleanos(persona){
+    return {
+        ...persona,
+        edad: persona.edad + 1
+    }
+}
---
+++ clase9.js
@@ -0,0 +1,9 @@
+var x = 4, y = '4';
+
+var hiram = {
+    nombre: 'hiram'
+}
+
+var otraPersona = {
+    nombre: 'hiram'
+}

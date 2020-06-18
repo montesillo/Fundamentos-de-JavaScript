2abe2759551a2972b42745323c5f8451ddca6364
Hiram Montesillo <montesillo25@gmail.com>
2020-06-17 20:55:08 -0500

    Clase 34

---
+++ clase34.js
@@ -0,0 +1,23 @@
+const API_URL = 'https://swapi.dev/api/';
+const PEOPLE_URL = 'people/:id';
+
+const opts = {crossDomain: true };
+
+function obtenerPersonaje(id, callback){
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+    $.get(url, opts, callback).fail(function (){
+        console.log(`Sucedío un error, no se pudo obtener el personaje ${id}`)
+    });
+}
+
+obtenerPersonaje(1, function (personaje) {
+    console.log(`Hola yo soy, ${personaje.name}`);
+
+    obtenerPersonaje(2, function (personaje){
+        console.log(`Hola yo soy, ${personaje.name}`);
+
+        obtenerPersonaje(3, function (personaje){
+            console.log(`Hola yo soy, ${personaje.name}`);
+        });
+    });
+});

19d16daa306afa643c0a2c758a4709e8eabb6bcd
Hiram Montesillo <montesillo25@gmail.com>
2020-06-21 21:41:51 -0500

    Clase 39

---
+++ clase39.js
@@ -0,0 +1,59 @@
+const API_URL = 'https://swapi.dev/api/';
+const PEOPLE_URL = 'people/:id';
+
+const opts = {crossDomain: true };
+
+function obtenerPersonaje(id){
+    return new Promise((resolve, reject) =>{
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+        $.get(url, opts, function (data){
+            resolve(data);
+        })
+        .fail(() => reject(id));
+    });
+}
+
+function onError(id){
+    console.log(`Sucedió un error al obtener el personaje ${id}`);
+}
+
+async function obtenerPersonajes(){
+var ids = [1, 2, 3, 4, 5, 6, 7];
+var promesas = ids.map(id => obtenerPersonaje(id));
+    try {
+        var personajes = await Promise.all(promesas);
+        console.log(personajes);
+
+    }catch(id){
+        onError(id);
+    }
+}
+
+obtenerPersonajes()
+
+// obtenerPersonaje(1).then(personaje => {
+//     console.log(`El personaje 1 es ${personaje.name}`)
+//     return obtenerPersonaje(2);
+// }).then(personaje => {
+//     console.log(`El personaje 2 es ${personaje.name}`)
+//     return obtenerPersonaje(3);
+// }).then(personaje => {
+//     console.log(`El personaje 3 es ${personaje.name}`)
+//     return obtenerPersonaje(4);
+// }).then(personaje => {
+//     console.log(`El personaje 4 es ${personaje.name}`)
+//     return obtenerPersonaje(5);
+// }).then(personaje => {
+//     console.log(`El personaje 5 es ${personaje.name}`)
+//     return obtenerPersonaje(6);
+// }).then(personaje => {
+//     console.log(`El personaje 6 es ${personaje.name}`)
+//     return obtenerPersonaje(7);
+// }).then(personaje => {
+//     console.log(`El personaje 7 es ${personaje.name}`)
+// })
+// .catch(onError)
+
+// obtenerPersonaje(1, function (personaje) {
+//     console.log(`Hola yo soy, ${personaje.name}`);
+// });
